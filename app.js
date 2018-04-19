09ebc8d9bc59a3bb0e299bc0c667279b48f332fd
Edgar56 <edgaras.chalilovas@gmail.com>
2018-04-19 22:19:35 +0300

    Ended 10 section about node js, started CMS projection section, first step did, now other steps to improve project

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
+//Mongodb with mongoose
+mongoose.connect('mongodb://localhost:27017/cms').then(db=>{
+    console.log('MONGO connected');
+}).catch(error => console.log("Could not connect: " + error));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Set View Engine
 
 app.engine('handlebars', exphbs({defaultLayout: 'home'}));
 app.set('view engine','handlebars');
 
-const main = require('./routes/home/main');
+// Load Routes
 
-app.use('/',main);
+const home = require('./routes/home/index');
+const admin = require('./routes/admin/index');
+const posts = require('./routes/admin/posts');
+
+//Use routes
+
+app.use('/',home);
+app.use('/admin',admin);
+app.use('/admin/posts',posts);
 
 app.listen(4500, () => {
     console.log(`Listening  on port 4500`);
